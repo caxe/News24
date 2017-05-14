58a89f70922d6bcc6f943fe2450b2085c4823917
Yordan Stoychev <caxe@abv.bg>
2017-05-14 14:55:46 +0300

    Minor changes.

--- News24/Controllers/HomeController.cs
+++ News24/Controllers/HomeController.cs
@@ -15,14 +15,7 @@ namespace News24.Controllers
 
 		public ActionResult About()
 		{
-			ViewBag.Message = "Your application description page.";
-
-			return View();
-		}
-
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
+			ViewBag.Message = "Website about news";
 
 			return View();
 		}
