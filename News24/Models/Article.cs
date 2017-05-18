3bb8af6cecb5d6f2d3d8ae4b56f480047135bfc1
Yordan Stoychev <caxe@abv.bg>
2017-05-18 13:26:40 +0300

    Added models for the news.

---
+++ News24/Models/Article.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace News24.Models
+{
+	public class Article
+	{
+		public string Author { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Url { get; set; }
+		public string UrlToImage { get; set; }
+		public DateTime PublishedAt { get; set; }
+	}
+
+	public partial class Rootobject
+	{
+		public Article[] Articles { get; set; }
+	}
+}
---
+++ News24/Models/Source.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace News24.Models
+{
+	public class Source
+	{
+		public string Name { get; set; }
+		public string Id { get; set; }
+	}
+
+	public partial class Rootobject
+	{
+		public Source[] Sources { get; set; }
+	}
+}
